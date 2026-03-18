b34d668e855a1744f226119ce1953a3c5b5bb9cd
Johnjacob12345 <162513125+Johnjacob12345@users.noreply.github.com>
2026-03-17 22:41:24 -0700

    Updated camera position

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,9 +4,6 @@
 
 package frc.robot;
 
-// import com.ctre.phoenix6.signals.InvertedValue;
-// import com.ctre.phoenix6.signals.NeutralModeValue;
-// import com.ctre.phoenix6.signals.SensorDirectionValue;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
@@ -17,18 +14,8 @@ import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.util.Units;
 
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide
- * numerical or boolean
- * constants. This class should not be used for any other purpose. All constants
- * should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>
- * It is advised to statically import this class (or one of its inner classes)
- * wherever the
- * constants are needed, to reduce verbosity.
- */
+
+
 public final class Constants {
 
     public static final class Subsystems {
@@ -56,7 +43,7 @@ public final class Constants {
             // Speeds
             public static final double kMaxShooterSpeedOut1 = 0.85;// Shooter =85%
             public static final double kMaxShooterSpeedOut2 = 0.76;// Shooter = 76%
-            public static final double kShooterTargetSpeed1 = 3427.0; // Target speed in RPM for the shooter
+            public static final double kShooterTargetSpeedUpper = 3427.0; // Target speed in RPM for the shooter
         }
 
         public static final class Climber {
@@ -115,10 +102,10 @@ public final class Constants {
             
 
             public static final Transform3d kCameraToRobot = new Transform3d(
-                    -0.1016, // forward from robot center
+                    0.1524, // forward from robot center     
                     0.0, // left/right camera is centered
-                    0.5588, // up from floor
-                    new edu.wpi.first.math.geometry.Rotation3d(0, 0.17453293, 0));
+                    0.2159, // up from floor
+                    new edu.wpi.first.math.geometry.Rotation3d(0, 0.0, 0));
                                                                                             //3.14159265
         }
 
